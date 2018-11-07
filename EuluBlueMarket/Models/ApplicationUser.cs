9e428c741486343a65c3b7ae4690063b594991ea
Eulu <lev01b27@gmail.com>
2018-11-07 10:44:41 +0300

    ErrorName

---
+++ EuluBlueMarket/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EuluBlueMarket.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name{ get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        List<MarketItem> lots { get; set; }
+        public ApplicationUser()
+        {
+        }
+    }
+}
---
+++ EuluBlueMarket/Models/MarketModels/UserDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EuluBlueMarket.Models
+{
+    public class UserDetails
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<MarketItem> Lots;
+    }
+}
